56b9f9a3e4fc853caf95bf0588202a5df81bf879
3amMcspicy <kc3265@columbia.edu>
2021-02-22 16:17:38 +0800

    setup to install the module

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+"""
+Install records package
+"""
+
+from setuptools import setup
+
+setup(
+    name="records",
+    version="0.0.1",
+    packages =[],
+    entry_points={
+    }
+)
